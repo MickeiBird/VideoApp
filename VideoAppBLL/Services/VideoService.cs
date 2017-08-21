cd18117000426801618c326e43ed24a350353126
MickeiBird <madmofoforeal@gmail.com>
2017-08-21 11:45:17 +0200

    New version
    Not yet running but will do tomorrow

---
+++ VideoAppBLL/Services/VideoService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VideoAppEntity;
+
+namespace VideoAppBLL.Services
+{
+    public class VideoService : IVideoService
+    {
+        public Video Create(Video vid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Video Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Video Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Video> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Video Update(Video vid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VideoAppEntity/Video.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoAppEntity
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Genre { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+
+        public Video(int Id, string Name, string Genre, int Year)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Genre = Genre;
+            this.Year = Year;
+        }
+
+        public  Video()
+        {
+
+        }
+
+    }
+}
